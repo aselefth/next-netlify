fe868b8c9a4336d528217e713cd5ce78c053fa78
a.evstratov <andrei200287@gmail.com>
2023-08-30 12:06:20 +0300

    static data

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
 import { Task } from "@/components/Task";
 import { Task as TaskType } from "@prisma/client";
+import { tasks as mockTasks } from "@/mockdata";
 
 export default async function Home() {
-	const tasksRes = await fetch(`http://localhost:3001/api/tasks`);
+	// const tasksRes = await fetch(`http://localhost:3001/api/tasks`);
 
-	const { tasks }: { tasks: TaskType[] } = await tasksRes.json();
+	// if (!tasksRes.ok) {
+	// 	return (
+	// 		<h1 className="w-full h-full text-3xl flex items-center justify-center font-semibold text-white/60">
+	// 			Нет информации
+	// 		</h1>
+	// 	);
+	// }
+
+	// const { tasks }: { tasks: TaskType[] | undefined } = await tasksRes.json();
 
 	return (
 		<div className="w-full flex flex-col items-center gap-4">
-			{tasks.length ? (
-				tasks.map((task) => <Task key={task.id} task={task} />)
+			{mockTasks?.length ? (
+				mockTasks.map((task) => <Task key={task.id} task={task} />)
 			) : (
 				<h1 className="w-full h-full text-3xl flex items-center justify-center font-semibold text-white/60">
 					Нет информации
